48814ff6ff85605823969dc3e75b11dbb415533f
pavel.sheinfeld <pavel.sheinfeld@gmail.com>
2021-03-10 18:34:28 +0000

    init fix

--- cloud-init/spark.sh
+++ cloud-init/spark.sh
@@ -11,8 +11,8 @@ sudo tar -xvzf spark-3.0.2-bin-hadoop2.7.tgz
 sudo mv /opt/spark-3.0.2-bin-hadoop2.7/ /opt/spark/
 sudo rm /opt/spark-3.0.2-bin-hadoop2.7.tgz
 
-sudo echo "export SPARK_HOME=\opt\spark" >> /home/$local_user/.bashrc
-sudo echo "export PATH=$SPARK_HOME/bin:$PATH" >> /home/$local_user/.bashrc
+sudo echo "export SPARK_HOME=/opt/spark" >> /home/$local_user/.bashrc
+sudo echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> /home/$local_user/.bashrc
 
 
 #python
@@ -20,8 +20,10 @@ sudo yum install -y oracle-epel-release-el7 oracle-release-el7
 sudo yum install -y python3
 
 #jupiter
-sudo pip3 install --upgrade pip
-sudo pip3 install jupyter
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install pyspark
+sudo python3 -m pip install jupyter
+
 
 echo "export PYSPARK_DRIVER_PYTHON=jupyter" >> /home/$local_user/.bashrc
 echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook'" >> /home/$local_user/.bashrc
@@ -30,6 +32,7 @@ echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook'" >> /home/$local_user/.bashrc
 
 #public access
 jupyter notebook --generate-config
+sudo mv ~/.jupyter/jupyter_notebook_config.py /home/$local_user/.jupyter/jupyter_notebook_config.py
 sed -i "s/#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '*'/" /home/$local_user/.jupyter/jupyter_notebook_config.py
 
 sudo firewall-cmd --permanent --zone=public --add-port=8888-8889/tcp
--- provision-resources.sh
+++ provision-resources.sh
@@ -25,7 +25,7 @@ echo "y" | oci os object put --bucket-name $object_storage_bucket_name --name ke
 
 #create pre-authenticated download links for private key
 echo -e "\e[1;32m => provision pre-authenticated path to auth key \e[0m"
-bject_storage_preauth_link="https://objectstorage.REGION.oraclecloud.com/PATH"
+object_storage_preauth_link="https://objectstorage.REGION.oraclecloud.com/PATH"
 time_expires=$(date -d "+7 days" +"%Y-%m-%dT%H:%MZ")
 access_uri=$(oci os preauth-request create --name "download key" --access-type ObjectRead --bucket-name $object_storage_bucket_name --time-expires $time_expires --object-name "keys/id_rsa" --query data.\"access-uri\")
 home_region=$(oci iam region-subscription list --raw-output --query "data [?\"is-home-region\"].\"region-name\" | [0]" )
